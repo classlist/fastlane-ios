6e6cebab66a28b522041efd1a8ebf837368561d1
Kajendran Pulendran <kajendran.pulendran@hotmail.com>
2019-09-30 14:37:44 +0100

    Set up
    Set up Hello World Project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Kajendran Pulendran on 30.09.19.
+//  Copyright © 2019 Kajendran Pulendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
